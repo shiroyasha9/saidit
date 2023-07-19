e457352b40e6538cc3e5f5a27149f85453059da0
Mubin Ansari <mubinansari910@gmail.com>
2023-07-19 21:07:51 +0530

    Add conditional rendering for user account nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
+import { getAuthSession } from "@/lib/auth";
 import Link from "next/link";
 import { Icons } from "./Icons";
+import UserAccountNav from "./UserAccountNav";
 import { buttonVariants } from "./ui/Button";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getAuthSession();
+
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2 '>
       <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
@@ -12,9 +16,19 @@ const Navbar = () => {
             Saidit
           </p>
         </Link>
-        <Link href='/sign-in' className={buttonVariants()}>
-          Sign in
-        </Link>
+        {session?.user ? (
+          <UserAccountNav
+            user={{
+              name: session.user.name,
+              image: session.user.image,
+              email: session.user.email,
+            }}
+          />
+        ) : (
+          <Link href='/sign-in' className={buttonVariants()}>
+            Sign in
+          </Link>
+        )}
       </div>
     </div>
   );
