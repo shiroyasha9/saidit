cbaee587ad7c785be0107119592446d64878f757
Mubin Ansari <mubinansari910@gmail.com>
2023-07-19 22:31:29 +0530

    Add close modal button component

---
+++ src/components/CloseModal.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { FC } from "react";
+import { Button } from "./ui/Button";
+
+const CloseModal: FC = () => {
+  const router = useRouter();
+  return (
+    <Button
+      variant='subtle'
+      className='h-6 w-6 p-0 rounded-md'
+      aria-label='close modal'
+      onClick={router.back}
+    >
+      <X className='h-4 w-4' />
+    </Button>
+  );
+};
+
+export default CloseModal;
