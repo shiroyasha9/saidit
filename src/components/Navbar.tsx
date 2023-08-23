bbdaf3249665c6e7c7c9b057537e36dcb632c5dd
Mubin Ansari <mubinansari910@gmail.com>
2023-08-24 01:49:22 +0530

    Update saidit logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { getAuthSession } from "@/lib/auth";
+import { Bot } from "lucide-react";
 import Link from "next/link";
-import { Icons } from "./Icons";
 import UserAccountNav from "./UserAccountNav";
 import { buttonVariants } from "./ui/Button";
 
@@ -8,11 +8,11 @@ const Navbar = async () => {
   const session = await getAuthSession();
 
   return (
-    <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2 '>
-      <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
-        <Link href='/' className='flex gap-2 items-center'>
-          <Icons.logo className='w-8 h-8 sm:h-6 sm:w-6' />
-          <p className='hidden text-zinc-700 text-sm font-medium md:block'>
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2 ">
+      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+        <Link href="/" className="flex gap-2 items-center">
+          <Bot className="w-8 h-8 sm:h-6 sm:w-6" />
+          <p className="hidden text-zinc-700 text-sm font-medium md:block">
             Saidit
           </p>
         </Link>
@@ -25,7 +25,7 @@ const Navbar = async () => {
             }}
           />
         ) : (
-          <Link href='/sign-in' className={buttonVariants()}>
+          <Link href="/sign-in" className={buttonVariants()}>
             Sign in
           </Link>
         )}
