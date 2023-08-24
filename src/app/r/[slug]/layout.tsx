ef9f5893b632f35369b6abb962097fe1cfe6d66d
Mubin Ansari <mubinansari910@gmail.com>
2023-08-24 18:36:08 +0530

    Add responsive fixes

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -30,15 +30,15 @@ const Layout = async ({
   const subscription = !session?.user
     ? undefined
     : await db.subscription.findFirst({
-        where: {
-          user: {
-            id: session.user.id,
-          },
-          subreddit: {
-            name: params.slug,
-          },
+      where: {
+        user: {
+          id: session.user.id,
         },
-      });
+        subreddit: {
+          name: params.slug,
+        },
+      },
+    });
 
   const isSubscribed = !!subscription;
 
@@ -59,7 +59,7 @@ const Layout = async ({
       <div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
           <div className="flex flex-col col-span-2 space-y-6">{children}</div>
-          <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last ">
+          <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
             <div className="px-6 py-4">
               <p className="font-semibold py-3">About r/{subreddit.name}</p>
             </div>
